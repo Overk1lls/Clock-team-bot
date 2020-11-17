6b3f38fdfeb5bdaf8b31989cebf558909f995cda
Yury <Ov3RforDream@gmail.com>
2020-11-17 17:37:25 +0200

    bot functional updated and optimized

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const { google } = require('googleapis');
 const readline = require('readline');
-
+const { MongoClient } = require('mongodb');
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 
 const TOKEN_PATH = 'token.json';
@@ -10,29 +10,44 @@ const TOKEN_PATH = 'token.json';
 require('dotenv').config();
 
 const bot = new Discord.Client();
+const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URIS[0]);
+let database;
 
-const client_secret = process.env.CLIENT_SECRET;
-const client_id = process.env.CLIENT_ID;
-const redirect_uris = process.env.REDIRECT_URIS;
+start();
 
-const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+async function start() {
+    bot.once('ready', () => {
+        console.log(`${bot.user.username} ready`);
+    });
+    
+    setGoogleAuthToken();
 
-// channels list
-const channels = [ '', '' ];
-// boosters list
-const boosters = [ '', '', '', '', '', '', '' ];
+    try {
+        const client = await MongoClient.connect(
+            process.env.MONGO_DB,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        );
+        database = client.db();
+    } catch (err) {
+        console.log(`DB Constructor error thrown: ${err}`);
+        process.exit(1);
+    }
+}
 
-function setToken() {
+function setGoogleAuthToken() {
     fs.readFile(TOKEN_PATH, (err, token) => {
         if (err) {
-            getNewToken(oAuth2Client);
-            setToken();
+            getGoogleAuthToken(oAuth2Client);
+            setGoogleAuthToken();
         }
         oAuth2Client.setCredentials(JSON.parse(token));
     })
 }
 
-function getNewToken(oAuth2Client) {
+function getGoogleAuthToken(oAuth2Client) {
     const authUrl = oAuth2Client.generateAuthUrl({
       access_type: 'offline',
       scope: SCOPES,
@@ -55,58 +70,117 @@ function getNewToken(oAuth2Client) {
     });
 }
 
-function accessSpreadsheet(message, auth) {
+function calculateSalary(message, auth) {
     const sheets = google.sheets({ version: 'v4', auth });
     let salary = 0;
+    var booster = message.content.split(' ')[1];
     
-    sheets.spreadsheets.values.get({ spreadsheetId: process.env.GOOGLE_SPREADSHEET, range: 'E2:I100', majorDimension: 'ROWS' }, (err, res) => {
-        if (err) return console.log('THE API RETURNED: ' + err);
+    sheets.spreadsheets.values.get({
+        spreadsheetId: process.env.GOOGLE_SPREADSHEET,
+        range: process.env.GOOGLE_SPREADSHEET_RANGE,
+        majorDimension: process.env.GOOGLE_SPREADSHEET_DIMENSION
+    }, async (err, res) => {
+        if (err) {
+            let msg = 'Something went wrong. An error occurred!';
+            message.guild ? botSendToChannel(message, msg) : botSendToAuthor(message, msg);
+            return console.log(`THE GOOGLE SHEETS API RETURNED: ${err}`);
+        }
 
         const rows = res.data.values;
         if (rows.length) {
             rows.map((row) => {
                 if (row != '') {
-                    var boosters = row[0].split(" ");
+                    var boosters = row[0].split(' ');
                     for (var i = 0; i < 4; i++) {
-                        if (boosters[i] && boosters[i].toUpperCase() === message.content.toUpperCase()) {
+                        if (boosters[i] && boosters[i].toUpperCase() === booster.toUpperCase()) {
                             if (row[4]) salary += parseInt(row[4]);
                         }
                     }
                 }
             });
-            
             sendSalary(message, salary);
         } else {
-            console.log("NO DATA FOUND");
+            let msg = 'Something went wrong. An error occurred!';
+            message.guild ? botSendToChannel(message, msg) : botSendToAuthor(message, msg);
+            return console.log('NO DATA FOUND IN THE GOOGLE SHEET');
         }
     });
     
 }
 
 function sendSalary(message, salary) {
-    let response = `${message.content.toUpperCase()}'s salary: ${salary}`;
+    var booster = message.content.split(' ')[1];
+    let response = `${booster.toUpperCase()}'s salary: ${salary}`;
 
     if (!message.guild) {
         message.author.send(response);
     } else bot.channels.cache.get(message.channel.id).send(response);
 }
 
-bot.once('ready', () => {
-    setToken();
-    console.log('ready');
-});
+function botSendToChannel(message, msg) {
+    return bot.channels.cache.get(message.channel.id).send(msg);
+}
 
-bot.on('message', (message) => {
-    if (!message.author.bot)
-        console.log('Received a message: ' + message.content + (message.guild ? `, guild: ${message.guild.name}, channel: ${message.channel.name}` : `, author: ${message.author.username}`));
-    else return;
-    if (channels.includes(message.channel.id) || !message.guild) {
-        if (boosters.includes(message.content.toUpperCase())) {
-            accessSpreadsheet(message, oAuth2Client);
+function botSendToAuthor(message, msg) {
+    return message.author.send(msg);
+}
+
+bot.on('message', async (message) => {
+    if (message.author.bot) return;
+    
+    try {
+        let content = message.content;
+        let author = message.author.username;
+        let channel_id = message.channel.id;
+
+        if (!message.guild) {
+            console.log(`Received a Direct Message: ${content}, author: ${author}`);
         }
+
+        if (content.startsWith('!booster')) {
+            let booster = content.split(' ')[1];
+            if (booster && booster != '') {
+                if (await database.collection('boosters').findOne({ name: booster.toUpperCase() })) {
+                    let msg = `This booster (${booster.toUpperCase()}) already exists in the database`;
+                    return message.guild ? botSendToChannel(message, msg) : botSendToAuthor(message, msg);
+                }
+                let newBooster = {
+                    name: `${booster.toUpperCase()}`,
+                    author: `${author}`
+                };
+                await database.collection('boosters').insertOne(newBooster);
+            }
+        } else if (content.startsWith('!channel')) {
+            let channel = message.content.split(' ')[1];
+            if (channel && channel != '') {
+                if (await database.collection('channels').findOne({ id: channel_id })) {
+                    let msg = `This channel (${channel}) already exists in the database`;
+                    return message.guild ? botSendToChannel(message, msg) : botSendToAuthor(message, msg);
+                }
+                let newChannel = {
+                    id: `${channel_id}`,
+                    name: `${bot.channels.cache.get(channel).name}`,
+                    guild: `${bot.channels.cache.get(channel).guild}`,
+                    author: `${author}`
+                }
+                await database.collection('channels').insertOne(newChannel);
+            }
+        } else if (content.startsWith('!check')) {
+            let booster = message.content.split(' ')[1];
+            if (booster && booster != '') {
+                if (await database.collection('boosters').findOne({ name: booster.toUpperCase() })) {
+                    calculateSalary(message, oAuth2Client);
+                } else {
+                    let msg = `Booster (${booster}) not found. If you want to check, add them by writing !booster nickname`;
+                    return message.guild ? botSendToChannel(message, msg) : botSendToAuthor(message, msg);
+                }
+            }
+        }
+    } catch (err) {
+        let msg = 'Something went wrong. An error occurred!';
+        message.guild ? botSendToChannel(message, msg) : botSendToAuthor(message, msg);
+        return console.log(`An error thrown while processing the message: ${err}`);
     }
 });
 
 bot.login(process.env.DISCORD_BOT_TOKEN);
-
-module.exports = bot;
